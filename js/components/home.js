7b7c4fe0f7299280efbc9d05e65b152c8fb6c936
Adam Fairhead <adam@adamfairhead.com>
2016-12-10 11:10:28 -0500

    Update Start page to AC

--- js/components/home.js
+++ js/components/home.js
@@ -1,3 +1,3 @@
 $(function() {
-  $('._form_26 style, ._form_28 style, ._form_30 style').remove();
+  $('._form_26 style, ._form_28 style, ._form_30 style, ._form_32 style').remove();
 });
--- js/production.js
+++ js/production.js
@@ -2901,5 +2901,5 @@ $(document).ready(function(){
 });
 
 $(function() {
-  $('._form_26 style, ._form_28 style, ._form_30 style').remove();
+  $('._form_26 style, ._form_28 style, ._form_30 style, ._form_32 style').remove();
 });
